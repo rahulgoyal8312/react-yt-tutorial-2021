45f9ff62f7426be528f25c4e3df6845b77ce908a
Rahul Goyal <root@Rahuls-MacBook-Pro.local>
2021-05-29 21:25:18 +0530

    initial setup and sidebar created

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
+
+  const sidebarList = [
+    {
+      name: "Expenses",
+      path: "/",
+      icon: "insights"
+    },
+    {
+      name: "Add Expense",
+      path: "/add",
+      icon: "note_add"
+    }
+  ]
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flexbox">
+      <div className="sidebar-container">
+        <div className="sidebar-container__brand">
+          Expense Tracker
+        </div>
+        <div className="sidebar-container__list">
+          <ul>
+            <li>
+              <a href={sidebarList[0].path} className="active">
+                <span className="material-icons">{sidebarList[0].icon}</span>
+                <span>{sidebarList[0].name}</span>
+              </a>
+            </li>
+            <li>
+              <a href={sidebarList[1].path}>
+                <span className="material-icons">{sidebarList[1].icon}</span>
+                <span>{sidebarList[1].name}</span>
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
+
+  // return React.createElement('div', { className: "parent" }, React.createElement('div', {}, title), React.createElement('p', {}, 'Content to be...'))
 }
 
 export default App;
